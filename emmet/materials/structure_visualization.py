8e9bd820b2a0c9f946ad40735043441562c16c92
Tyler <tylerhuntington222@lbl.gov>
2019-05-31 17:56:14 -0700

    Add docstring to StructureVisualization class.

--- emmet/materials/structure_visualization.py
+++ emmet/materials/structure_visualization.py
@@ -1,7 +1,13 @@
 from typing import List, Dict, Any
 from pydantic import BaseModel
 
+
 class StructureVisualization(BaseModel):
+    """
+    Subclass of `pydantic.Basemodel` used for schema validation
+    of Stores built by `VisualizationBuilder`.
+    """
+    
     scene: Dict[str, Any]
     legend: Dict[str, dict]
     settings: Dict[str, Any]
